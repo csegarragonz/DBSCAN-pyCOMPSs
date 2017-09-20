8eb4466fe5abca2b027691256cfa1029556d6688
Carlos Segarra <carlossegarragonzalez@gmail.com>
2017-09-20 09:42:52 +0200

    Corrected styling

--- ext_versions/DBSCAN_Seq.py
+++ ext_versions/DBSCAN_Seq.py
@@ -1,11 +1,4 @@
 # Reference paper: http://www.aaai.org/Papers/KDD/1996/KDD96-037.pdf
-# Ideas:
-# 1) RegionQuery por fragmentos de puntos y reduce para juntar todos los puntos resultantes.
-#    Posible problema: expandCluster tambien podria hacerse por fragmentos de datos.
-#    Pero entonces regionQuery no podria ser tarea (nesting no soportado)
-# 2) El calculo mas costoso son las distancias. Precalcular las distancia por fragmentos de datos.
-#    consultar esos datos para el resto de calculos. Otra opcion es usar kd-trees para evitar el calculo de distancias.
-# 3) Para regionQuery, subdividir el espacio de puntos y llamar a regionQuery solo con bloques contiguos
 from collections import defaultdict
 import numpy as np
 import random
@@ -16,17 +9,19 @@ points = defaultdict(int)
 
 
 def normalizeData(dataFile):
-	"""
-	Given a dataset, divide each dimension by its maximum obtaining data in the range [0,1]^n
-	:param	dataFile: name of the original data file.
-	:return newName: new name of the data file containing the normalized data.
-	"""
-        dataset=np.loadtxt(dataFile)
-        normData=np.where(np.max(dataset, axis=0)==0, dataset, dataset*1./np.max(dataset, axis=0))
-        newName=dataFile[dataFile.find('.')+1:dataFile.rfind('.')]
-	newName='.'+newName+'n.txt'
-	np.savetxt(newName,normData)
-	return newName
+    """
+    Given a dataset, divide each dimension by its maximum obtaining data in
+     the range [0,1]^n
+    :param	dataFile: name of the original data file.
+    :return newName: new name of the data file containing the normalized data.
+    """
+    dataset = np.loadtxt(dataFile)
+    normData = np.where(np.max(dataset, axis = 0) == 0, dataset, 
+        dataset*1./np.max(dataset, axis = 0))
+    newName = dataFile[dataFile.find('.') + 1:dataFile.rfind('.')]
+    newName = '.'+newName + 'n.txt'
+    np.savetxt(newName,normData)
+    return newName
 
 def DBSCAN(dataset, epsilon, minPoints):
     """
@@ -57,15 +52,15 @@ def expandCluster(point, neighborPts, c, epsilon, minPoints):
     :param epsilon: minimum distance
     :param minPoints: min number of points to not be considered noise
     """
-    cluster[c].append(point)  # add point to the cluster
-    points[str(point)] = c    # assign the point to the cluster
+    cluster[c].append(point)  
+    points[str(point)] = c    
     for p in neighborPts:
         if not visited[str(p)]:
             visited[str(p)] = True
-            neighborPts2 = regionQuery(dataset, p, epsilon)  # find neighbours
+            neighborPts2 = regionQuery(dataset, p, epsilon)
             if len(neighborPts2) >= minPoints:
                 neighborPts += neighborPts2
-        if str(p) not in points:  # if the point is not assigned to another cluster...
+        if str(p) not in points:
             cluster[c].append(p)
             points[str(p)] = c
 
@@ -78,9 +73,9 @@ def regionQuery(dataset, point, epsilon):
     :param epsilon: minimum distance to become a neighbour
     :return: point neighbors
     """
-    distance = [np.linalg.norm(point-p) for p in dataset]  # improve: symmetric distance
-    # (p-point).all() check if the distance is not zero (because if it's 0 is the same point)
-    neighborPts = [p for i, p in enumerate(dataset) if (p-point).all() and distance[i] < epsilon]
+    distance = [np.linalg.norm(point-p) for p in dataset]
+    neighborPts = [p for i, p in enumerate(dataset) if (p-point).all() and distance[i]
+        < epsilon]
     return neighborPts
 
 
@@ -110,7 +105,8 @@ if __name__ == "__main__":
         colours = [hex for (name, hex) in colors.cnames.iteritems()]
         fig, ax = plt.subplots()
         for c in cluster:
-            ax.scatter([p[0] for p in cluster[c]], [p[1] for p in cluster[c]], color=colours[c])
+            ax.scatter([p[0] for p in cluster[c]], [p[1] for p in cluster[c]], 
+                color=colours[c])
         plt.show()
         plt.savefig('dbscan2.png')
 
