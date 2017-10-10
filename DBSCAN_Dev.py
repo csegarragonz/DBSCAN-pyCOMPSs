2c9e4ba444c6c81ddd89c4060dd9a0a32437d2ee
Carlos Segarra <carlossegarragonzalez@gmail.com>
2017-10-10 13:40:03 +0200

    Implemented more functions. To this point, syncing is working. Starting to work on how to label clusters more efficiently.

--- DBSCAN_Dev.py
+++ DBSCAN_Dev.py
@@ -2,9 +2,9 @@
 #carlos.segarra @ bsc.es
 
 #Imports
-from pycompss.api.task import task
-from pycompss.api.parameter import *
-from pycompss.api.api import compss_wait_on
+#from pycompss.api.task import task
+#from pycompss.api.parameter import *
+#from pycompss.api.api import compss_wait_on
 from collections import defaultdict
 from classes.cluster import Cluster
 from classes.DS import DisjointSet
@@ -15,26 +15,43 @@ import sys
 import math
 import random
  
-#This might change to include sigmoid normalisation, rather than dividing by
-#the maximum value what might result in numerical inestability.
-#Complete migration to NumPy?
-def normalize_data(data_file):
-    """
-    Given a dataset, divide each dimension by its maximum obtaining data 
-    in the range [0,1]^n
-    :param dataFile: name of the original data file.
-    :return newName: new name of the data file containing the normalized 
-    data.
-    """
-    dataset = np.loadtxt(data_file)
-    norm_data = np.where(np.max(dataset, axis=0) == 0, dataset, 
-        dataset*1./np.max(dataset, axis=0))
-    new_name = data_file[data_file.find('.') + 1:data_file.rfind('.')]
-    new_name = '.' + new_name + 'n.txt'
-    np.savetxt(new_name,norm_data)
-    return new_name
+##This might change to include sigmoid normalisation, rather than dividing by
+##the maximum value what might result in numerical inestability.
+##Complete migration to NumPy?
+#def normalize_data(data_file):
+#    """
+#    Given a dataset, divide each dimension by its maximum obtaining data 
+#    in the range [0,1]^n
+#    :param dataFile: name of the original data file.
+#    :return newName: new name of the data file containing the normalized 
+#    data.
+#    """
+#    dataset = np.loadtxt(data_file)
+#    norm_data = np.where(np.max(dataset, axis=0) == 0, dataset, 
+#        dataset*1./np.max(dataset, axis=0))
+#    new_name = data_file[data_file.find('.') + 1:data_file.rfind('.')]
+#    new_name = '.' + new_name + 'n.txt'
+#    np.savetxt(new_name,norm_data)
+#    return new_name
 
-@task(data_pos = INOUT)
+#TRASH function
+def consume_data(dataset, filename):
+    data_to_load = np.loadtxt(filename)
+    for point in data_to_load:
+        i = int(point[0]*10)
+        j = int(point[1]*10)
+        dataset[i][j].append(point)
+    for i, row in enumerate(dataset):
+        for j, square in enumerate(row):
+            if len(square) > 0:
+                dataset[i][j] = np.vstack(square)
+            else:
+                dataset[i][j] = np.array([(np.random.uniform(low = float(i)/10
+                    , high = float(i+1)/10), np.random.uniform(low = 
+                    float(j)/10, high = float(j+1)/10))])
+    return dataset
+
+#@task(data_pos = INOUT)
 def init_data(data_pos, square, num_points):
     """
     FOR THE MOMENT IT ONLY WORKS IN A  10x10 MATRIX
@@ -45,169 +62,98 @@ def init_data(data_pos, square, num_points):
     only to this square. Square is an int and each number from right to left 
     denotes the cell number along that dimension (dim_n-1 dim_n-2 ... dim 0) 
     """
-    dim=len(str(square))
+    dim=len(square)
     data_pos = np.random.sample([num_points, dim])
     for i in range(dim):
-        pos_ind = square%10
-        data_pos[:,i] = data_pos[:,i]/10 + float(pos_ind)/10 
-        square = square/10 
-    
+        data_pos[:,i] = data_pos[:,i]/10 + float(square[i])/10 
+    #DELETE
+    return data_pos
 
-def partial_scan(corePoints, square, epsilon, minPoints, fragData, 
-                fragSize, numParts, rangeToEps):
-    """
-    Looks for all the core points (over minPoints neighbours) inside a 
-    certain square.
-    :param square: space region where core points are looked for.
-    :param epsilon: maximum distance between two points to be considered 
-    neighbours.
-    :param minPoints: minimum number of neighbours for a point to be 
-    considered core point.
-    :param fragData: dict containing space partition and its points.
-    :param fragSize: length of the side of each hypercube inside fragData.
-    :return:    list of all the core points found in square.  
-    """
-    dim = len(fragData[square][0])
-    pointSet = fragData[square]
-    pointSetReal = pointSet[:]
-    k=rangeToEps[square]
-    size = pow(10,len(str(fragSize+1)))
+#Only works in 2D and 10 squares per side
+def neigh_squares_query(square, epsilon):
+    #This could be modified to include only borders. 0.1 is the side size
+    dim = len(square)
+    neigh_squares = []
+    border_squares = int(max(epsilon/0.1, 1))
     perm = []
     for i in range(dim):
-        perm.append(range(-k[i],k[i] + 1))
+        perm.append(range(-border_squares, border_squares + 1))
     for comb in itertools.product(*perm):
-        current = square
+        current=[]
         for i in range(dim):
-            current = current + comb[i]*math.pow(size,i)
-        if current in fragData and current != square:
-            pointSet = pointSet + fragData[current]
-    for i in range(numParts):
-        tmpPS = [p for j,p in enumerate(pointSetReal) if j % numParts == 
-            i]
-        scan_task(corePoints[i], pointSet, tmpPS, epsilon, minPoints)
+            if square[i] + comb[i] in range(10):
+                current.append(square[i]+comb[i])
+        if len(current) == dim and current != square:
+            neigh_squares.append(current)
+    return neigh_squares    
 
-@task(corePoints = INOUT) 
-def scan_task(corePoints, pointSet, pointSetReal, epsilon, minPoints):
-    for point in pointSetReal:
-        neighbourPts = 0
-        for p in pointSet:
-            if np.linalg.norm(point - p) < epsilon:
-                neighbourPts = neighbourPts+1
-                if neighbourPts >= minPoints:
-                    corePoints.append(point)
-                    break  
+#@task(core_points_list = INOUT)
+def partial_scan(data, core_points_list, epsilon, min_points, *args):
+    neigh_points = np.vstack(args)
+    for point in data:
+        if np.sum((np.linalg.norm(neigh_points - point, axis = 1) - 
+            epsilon) < 0) > min_points:
+            core_points_list.append(point)
+    #DELETE
+    return core_points_list
 
-@task(clusters = INOUT) 
-def merge_cluster(clusters, corePoints, square, epsilon):
-    """
-    Given a list of core points, decides wether each core point forms a 
-    new cluster or belongs to an existing one.
-    :inout clusters:    list of clusters found.
-    :param corePoints:   list of all the core points found in square.
-    :param square:     working square.
-    :param epsilon:    input parameter.
-    """
-    for point in corePoints:
-        possibleClusters = []
+#@task(clusters = INOUT) 
+def merge_cluster(clusters, core_points, epsilon):
+    for point in core_points:
         for clust in clusters:
-            for clustP in clust.points:
-                if np.linalg.norm(point-clustP) < epsilon:
-                    possibleClusters.append(clust)
-                    if len(possibleClusters) > 1:
-                        clusters.remove(clust)
-                    break
-        if len(possibleClusters) > 0:
-            master = possibleClusters.pop(0)
-            master.addPoint(point)
-            for slave in possibleClusters:
-                master.merge(slave)
+            if np.sum((np.linalg.norm(clust - point, axis = 1) - 
+                epsilon) < 0) > 0:
+                clust.append(point)
+                break
         else:
-            #Class Cluster is defined at /classes/cluster.py
-            tmp = ([point],[square])
-            tmpc = Cluster()
-            tmpc.add(*tmp)
-            clusters.append(tmpc)
+            clusters.append([point])
+    for num, clust in enumerate(clusters):
+        np.vstack(clust)
+    #DELETE
+    return clusters
 
-def sync_clusters(clusters, epsilon, numParts):
-    """
-    Returns a matrix of booleans. Pos [i,j]=1 <=> clusters -i and -j 
-    should be merged.
-    :inout clusters: list of all clusters and their points.
-    :param numParts: list of clusters to be merged with the general list.
-    :param epsilon: input parameter, maximum distance under which two 
-    points are considered neighbours.
-    :return possibleClusters: result matrix.
-    """
-    possibleClusters = defaultdict(list)
-    n = 10
-    tmpDel = (n - len(clusters) % n) % n
-    if not len(clusters) % n ==0:
-        for i in range(n - len(clusters) % n): clusters.append(Cluster())
-    enable = [[[] for z in range(len(clusters)/n)] for w in 
-        range(len(clusters)/n)]
-    for i in range(len(clusters)/n):
-        tmp1 = [clusters[n*i + k].points for k in range(n)]
-        for j in range(len(clusters)/n):
-            tmp2=[clusters[n*j + k].points for k in range(n)]
-            sync_task(enable[i][j], tmp1, tmp2, epsilon)
-    a = time.time()
-    enable = compss_wait_on(enable)
-    print "Long Wait on Time: %.6f" % float(time.time()-a)
-    for i in range(len(clusters)/n): 
-        for j in range(len(clusters)/n):
-            for k in range(n):
-                for l in range(n):
-                    if enable[i][j][n*k +l]: 
-                        possibleClusters[i*n+k].append(j*n+l)
-    l = len(clusters)
-    for i in range(tmpDel):
-        possibleClusters.pop(l-1-i, None)
-        clusters.pop()    
-    return possibleClusters
-
-@task(enable = INOUT)
-def sync_task(enable, hosts, visits, epsilon):
-    """
-    Given two lists, checks wether the distance between the two sets is 
-    less than epsilon.
-    """
-    for host in hosts:
-        for visit in visits:
-            for p in host:
-                for point in visit:
-                    if np.linalg.norm(point-p) < epsilon:
-                        enable.append(1)
+#@task(adj_mat = INOUT)
+def sync_clusters(clusters, adj_mat, epsilon, *args):
+    adj_mat = [[] for _ in range(len(clusters))]
+    for num1, clust in enumerate(clusters):
+        for neigh_with_coord in args:
+            neigh_clusters = neigh_with_coord[0]
+            neigh_coord = neigh_with_coord[1]
+            for num2, neigh_clust in enumerate(neigh_clusters):
+                for point in clust:
+                    if np.sum((np.linalg.norm(neigh_clust - point, axis = 1) -
+                        epsilon) < 0) > 0:
+                        adj_mat[num1].append([neigh_coord[0], neigh_coord[1], 
+                            num2])
                         break
-                else: continue
-                break   
-            else: enable.append(0)
+    #DELETE
+    return adj_mat
 
-def update(clusters, possibleClusters, returnCluster):
-    """
-    Given a list of clusters, it reassigns each point to a cluster making
-    sure a point does not appear in more than one different clusters. If 
-    so, those clusters will be merged.
-    :param clusters: provisional cluster list result of the cluster 
-    expansion.
-    :return defClusters: final cluster list.
-    """
-    MF_set = DisjointSet(range(len(clusters)))
-    for i in possibleClusters:
-        for j in range(len(possibleClusters[i])-1):
-            MF_set.union(possibleClusters[i][j], possibleClusters[i][j+1])
-    a = MF_set.get()
-    if returnCluster:
-        defCluster = [Cluster() for _ in range(len(a))]
-        for i,lst in enumerate(a):
-            for elem in lst:
-                defCluster[i].merge(clusters[elem])
-        return defCluster
-    defCluster = [[] for _ in range(len(a))]
-    for i,lst in enumerate(a):
-        for elem in lst:
-            for point in clusters[elem].points:
-                defCluster[i].append(point)
-    return defCluster
+def unwrap_adj_mat(adj_mat):
+    cardinals = [[len(adj_mat[i][j]) for j in range(len(adj_mat[i]))] for i in
+        range(len(adj_mat))]
+    cum_sum = np.cumsum(cardinals, axis = 1)
+    for i in range(1, len(cum_sum) - 1):
+        cum_sum[i] += cum_sum[i-1][-1]
+    count = 0
+    links = defaultdict(list)
+    for i in range(len(adj_mat)):
+        for j in range(len(adj_mat[i])):
+            for k in range(len(adj_mat[i][j])):
+                links[count].append(count)
+                for num, l in enumerate(adj_mat[i][j][k]):
+                    clust_id = cum_sum[i][j] + num
+                    links[count].append(clust_id)
+                count += 1
+    return links
+
+def update(links_list):
+    mf_set = DisjointSet(range(len(links_list)))
+    for i in links_list:
+        for j in range(len(links_list[i])-1):
+            mf_set.union(links_list[i][j], links_list[i][j+1])
+    final_mf_set = mf_set.get()
+    return final_mf_set
 
 def expand_cluster(clusters, fragData, epsilon, minPoints, fragSize, 
                     numParts, rangeToEps):
@@ -239,7 +185,7 @@ def expand_cluster(clusters, fragData, epsilon, minPoints, fragSize,
                 clusters[i].addPoint(p) 
     return update(clusters, links, False)
             
-@task(clustPoints = INOUT)
+#@task(clustPoints = INOUT)
 def neigh_expansion(clustPoints, clust, fragData, fragSize, rangeToEps, 
                     epsilon):
     """
@@ -279,69 +225,52 @@ def neigh_expansion(clustPoints, clust, fragData, fragSize, rangeToEps,
                 clustPoints.append(point)
                 break
 
-def DBSCAN(data_file, fragSize, epsilon, minPoints, numParts):
-    """
-    Main DBSCAN algorithm.
-    :param dataFile: file where the data is stored.
-    :param fragSize: size used for space partition.
-    :param epsilon: maximum distance for two points to be considered 
-    neighbours.
-    :param minPoints: minimum number of neighbours for a point to be 
-    considered core point.
-    :param numParts: number of parts in which fragData is divided for 
-    processing.
-    :return defClusters: result of the application, dict where values 
-    are points corresponding to the <key> cluster.
-    """
+def DBSCAN(epsilon, min_points):
+    epsilon = float(epsilon)
+    min_points = int(min_points)
     start = time.time()
     #This variables are currently hardcoded
     num_grid_rows = 10
     num_grid_cols = 10
-    num_points = 100
+    num_points = 5
     dim = 2
-    dataset = np.empty([num_grid_rows, num_grid_cols, num_points, dim])
+    dataset = [[[] for _ in range(num_grid_cols)] for __ in
+        range(num_grid_rows)]
+#    dataset = np.empty([num_grid_rows, num_grid_cols, num_points, dim])
+#    for i in range(num_grid_rows):
+#        for j in range(num_grid_cols):
+            #This ONLY WORKS IN 2D
+            #init_data(dataset[i][j], [i,j], num_points)  
+            #DELETE
+#            dataset[i][j] = init_data(dataset[i][j], [i,j], num_points)  
+    dataset = consume_data(dataset, "./data/blobs_0to1.txt")
+    clusters = [[[] for _ in range(num_grid_cols)] for __ in 
+        range(num_grid_rows)]
+    core_points = [[[] for _ in range(num_grid_cols)] for __ in 
+        range(num_grid_rows)]
+    adj_mat = [[[] for _ in range(num_grid_cols)] for __ in 
+        range(num_grid_rows)]
     for i in range(num_grid_rows):
         for j in range(num_grid_cols):
-            #This ONLY WORKS IN 2D
-            square = i + 10*j
-            dataset[i][j] = init_data(dataset[i][j], square, num_points)  
+            neigh_sq_coord = neigh_squares_query([i,j], epsilon)
+            neigh_squares = []
+            for coord in neigh_sq_coord:
+                neigh_squares.append(dataset[coord[0]][coord[1]])
+#            partial_scan(dataset[i][j], core_points[i][j], epsilon, min_points,
+##                 *neigh_squares) 
+##            merge_cluster(clusters[i][j], core_points[i][j], epsilon)
+            #DELETE
+            core_points[i][j] = partial_scan(dataset[i][j], core_points[i][j], epsilon, min_points,
+                 *neigh_squares) 
+            clusters[i][j] = merge_cluster(clusters[i][j], core_points[i][j], epsilon)
+            neigh_clusters = []
+            for coord in neigh_sq_coord:
+                neigh_clusters.append([clusters[coord[0]][coord[1]], coord])
+            adj_mat[i][j] = sync_clusters(clusters[i][j], adj_mat[i][j], epsilon, 
+                *neigh_clusters) 
+##    adj_mat = compss_wait_on(adj_mat)
+    link_list = unwrap_adj_mat(adj_mat)
+    link_list = update(link_list)
 
-    print "Starting partial scan..."
-    clusters = [[[] for _ in range(len(fragData))] for __ in 
-        range(numParts)]
-    corePMatrix = [[[] for _ in range(numParts)] for __ in 
-        range(len(fragData))]
-    for i, (square,value) in enumerate(fragData.viewitems()):
-        partial_scan(corePMatrix[i], square, epsilon, minPoints, fragData,
-            fragSize, numParts, rangeToEps)
-        for j in range(numParts):
-            merge_cluster(clusters[j][i], corePMatrix[i][j], square, 
-                epsilon)
-    clusters = compss_wait_on(clusters)
-    print "Initial Proposal Finished"
-    iniP = time.time()
-    clusters = [clust for rows in clusters for squares in rows for clust 
-        in squares] 
-    print "Length of clusters found: "+str(len(clusters))
-    possibleClusters = sync_clusters(clusters, epsilon, numParts)
-    syncT = time.time()
-    print "Syncing Finished"
-    halfDefClusters = update(clusters, possibleClusters, True)
-    updateTime = time.time()
-    defClusters = expand_cluster(halfDefClusters, fragData, epsilon, 
-        minPoints, fragSize, numParts, rangeToEps)
-    print "Expansion Finished" 
-    print "DBSCAN Algorithm finished succesfully."
-    end = time.time()
-    print "Exec Times:"
-    print "----------------------------------"
-    print "Initial Proposal Time: \t %.6f" % float(iniP-start)
-    print "Syncing Time: \t \t  %.6f" % float(syncT-iniP)
-    print "Update Time: \t \t  %.6f" % float(updateTime-syncT)
-    print "Expand: \t \t  %.6f" % float(end-updateTime)
-    print "----------------------------------"
-    print "Time elapsed: \t \t  %.6f" % float(end-start)
-    print "Number of clusters found: "+str(len(defClusters))
-    sys.stdout.flush()
-    sys.stderr.flush()
-    return [defClusters, fragVec]
+    #What to do with the output?
+    return link_list
