49e689a9946023fe47a4cc9c5787b86e8a8eece6
Carlos Segarra <carlossegarragonzalez@gmail.com>
2017-11-02 16:07:24 +0100

    Functional version of the labeling cluster branch, merging to master

--- DBSCAN_Dev.py
+++ DBSCAN_Dev.py
@@ -3,7 +3,6 @@
 
 # Imports
 from pycompss.api.task import task
-# from pycompss.api.parameter import *
 from pycompss.api.parameter import INOUT
 from pycompss.api.api import compss_wait_on
 # from pycompss.api.api import barrier
@@ -12,10 +11,10 @@ from collections import defaultdict
 from classes.DS import DisjointSet
 from classes.Data import Data
 import itertools
-import time
+# import time
 import numpy as np
 import sys
-import math
+# import math
 
 
 @task(data_pos=INOUT)
@@ -61,16 +60,34 @@ def neigh_squares_query(square, epsilon):
     return neigh_squares
 
 
-@task(data=INOUT)
-def partial_scan(data, core_points_list, epsilon, min_points, *args):
+@task(data=INOUT, returns=defaultdict)
+def partial_scan(data, epsilon, min_points, *args):
     tmp_unwrap = [i.value[0] for i in args]
+    tmp_unwrap_2 = [i.value[1] for i in args]
     neigh_points = np.vstack(tmp_unwrap)
+    neigh_points_clust = np.concatenate(tmp_unwrap_2)
+    non_assigned = defaultdict(int)
     for num, point in enumerate(data.value[0]):
-        if np.sum((np.linalg.norm(neigh_points - point, axis=1) -
-                   epsilon) < 0) > min_points:
-            core_points_list.append(point)
+        poss_neigh = np.linalg.norm(neigh_points - point, axis=1) - epsilon < 0
+        neigh_count = np.sum(poss_neigh)
+        if neigh_count > min_points:
             data.value[1][num] = -1
-#    return data
+        elif neigh_count > 0:
+            tmp = []
+            for pos, proxim in enumerate(poss_neigh):
+                if proxim:
+                    # Adding three since later to detect core points we will
+                    # require value > -1 and -0 is > 1 and not to confuse it
+                    # with a noise point
+                    if neigh_points_clust[pos] == -1:
+                        data.value[1][num] = -(pos+3)
+                        break
+                    else:
+                        tmp.append(pos)
+            else:
+                non_assigned[num] = tmp
+    return non_assigned
+#    return data, non_assigned
 
 
 @task(data=INOUT, returns=int)
@@ -93,7 +110,6 @@ def merge_cluster(data, epsilon):
 
 @task(adj_mat=INOUT)
 def sync_clusters(data, adj_mat, epsilon, coord, neigh_sq_loc, *args):
-#    adj_mat = [[] for _ in range(max(adj_mat[0], 1))]
     nice_args = [[args[i], neigh_sq_loc[i]] for i in range(len(neigh_sq_loc))]
     nice_args.append([data, coord])
     for num1, point1 in enumerate(data.value[0]):
@@ -109,7 +125,6 @@ def sync_clusters(data, adj_mat, epsilon, coord, neigh_sq_loc, *args):
                         if (clust_ind > -1) and (adj_mat_elem not in
                                                  adj_mat[current_clust_id]):
                             adj_mat[current_clust_id].append(adj_mat_elem)
-                            print adj_mat
 #    return adj_mat
 
 
@@ -130,97 +145,36 @@ def unwrap_adj_mat(tmp_mat, adj_mat):
     return out
 
 
-def update(links_list, adj_mat):
-    mf_set = DisjointSet(range(len(links_list)))
-    for i in links_list:
-        for j in range(len(links_list[i])-1):
-            mf_set.union(links_list[i][j], links_list[i][j+1])
-    final_mf_set = mf_set.get()
-    return final_mf_set
-
-
-def expand_cluster(clusters, fragData, epsilon, minPoints, fragSize,
-                    numParts, rangeToEps):
-    """
-    Expands all clusters contained in a list of clusters. Expansion means
-    adding non-core points to the
-    already established clusters.
-    """
-    addToClust = [[[] for _ in range(numParts)] for x in
-        range(len(clusters))]
-    for numClust,clust in enumerate(clusters):
-        for k in range(numParts):
-            neigh_expansion(addToClust[numClust][k], clust, fragData,
-                fragSize, rangeToEps, epsilon)
-    addToClust = compss_wait_on(addToClust)
-    for i,m in enumerate(addToClust):
-        addToClust[i] = [j for k in m for j in k]
-    pointsToClust = defaultdict(list)
-    links = defaultdict(list)
-    for i,clust in enumerate(addToClust):
-        for p in addToClust[i]:
-            if str(p) in pointsToClust:
-                for c in pointsToClust[str(p)]:
-                    if not i in links[c]: links[c].append(i)
-                    if not c in links[i]: links[i].append(c)
-                pointsToClust[str(p)].append(i)
-            else:
-                pointsToClust[str(p)].append(i)
-                clusters[i].addPoint(p)
-    return update(clusters, links, False)
-
-##@task(clustPoints = INOUT)
-
-
-def neigh_expansion(clustPoints, clust, fragData, fragSize, rangeToEps,
-                    epsilon):
-    """
-    Given a cluster of core points, returns all the points lying in the
-    squares reachable with epsilon distance
-    from the clusters square.
-    :param cluster: cluster whose neighbour points will be added.
-    :param fragData: dict containing space partitioning.
-    :param epsilon: maximum distance between two points to be considered
-    neighbours.
-    :param minPoints: minimum number of neighbours for a point to be
-    considered core point.
-    :param fragSize: length of the side of each hypercube inside fragData.
-    """
-    squaresNot = clust.square[:]
-    pointSet = []
-    for sq in clust.square:
-        #Segur que no ho sumem dos cops aleshores?
-        pointSet += fragData[sq][:]
-        dim = len(fragData[sq][0])
-        k = rangeToEps[sq]
-        size = pow(10,len(str(fragSize+1)))
-        perm = []
-        for i in range(dim):
-            perm.append(range(-k[i],k[i]+1))
-        for comb in itertools.product(*perm):
-            current = sq
-            for i in range(dim):
-                current = current+comb[i]*math.pow(size,i)
-            if current in fragData and not(current in squaresNot):
-                pointSet = pointSet+fragData[current][:]
-                squaresNot.append(current)
-    tmp = [b for b in pointSet if not(b in clust.points)]
-    for i,point in enumerate(tmp):
-        for p in clust.points:
-            if np.linalg.norm(point-p) < epsilon:
-                clustPoints.append(point)
+@task(data=INOUT)
+def expand_cluster(data, epsilon, border_points, *args):
+    tmp_unwrap_2 = [i.value[1] for i in args]
+    neigh_points_clust = np.concatenate(tmp_unwrap_2)
+    for elem in border_points:
+        for p in border_points[elem]:
+            if neigh_points_clust[p] > -1:
+                data.value[1][elem] = neigh_points_clust[p]
                 break
+    for num, elem in enumerate(data.value[1]):
+        if elem < -2:
+            clust_ind = -1*elem - 3
+            data.value[1][num] = neigh_points_clust[clust_ind]
+#    return data
 
 
 def DBSCAN(epsilon, min_points):
     #   TODO: code from scratch the Disjoint Set
+    #   TODO: comment the code apropriately
+    #   TODO: remove all the hardcoded parts add a dim input argument
+
+    # Initial Definitions
     epsilon = float(epsilon)
     min_points = int(min_points)
-#    start = time.time()
     # This variables are currently hardcoded
     num_grid_rows = 10
     num_grid_cols = 10
     num_points_max = 100
+
+    # Data inisialitation
     centers = [[0.2, 0.3], [0.6, 0.7]]
     std = [[[0.01, 0], [0, 0.01]], [[0.01, 0], [0, 0.01]]]
     dataset = [[Data() for _ in range(num_grid_cols)] for __ in
@@ -230,20 +184,26 @@ def DBSCAN(epsilon, min_points):
             # This ONLY WORKS IN 2D
             init_data(dataset[i][j], [i, j], num_points_max,
                       centers, std)
-    core_points = [[[] for _ in range(num_grid_cols)] for __ in
-                   range(num_grid_rows)]
+
+    # Partial Scan And Initial Cluster merging
     adj_mat = [[[] for _ in range(num_grid_cols)] for __ in
                range(num_grid_rows)]
+    border_points = [[[] for _ in range(num_grid_cols)] for __ in
+                     range(num_grid_rows)]
     for i in range(num_grid_rows):
         for j in range(num_grid_cols):
             neigh_sq_coord = neigh_squares_query([i, j], epsilon)
             neigh_squares = []
             for coord in neigh_sq_coord:
                 neigh_squares.append(dataset[coord[0]][coord[1]])
-            partial_scan(dataset[i][j], core_points[i][j], epsilon, min_points,
-                         *neigh_squares)
+            # Use border points and adj_mat as INOUT instead of OUT
+            border_points[i][j] = partial_scan(dataset[i][j], epsilon,
+                                               min_points, *neigh_squares)
             adj_mat[i][j] = merge_cluster(dataset[i][j],  epsilon)
+
+    # Cluster Synchronisation
     adj_mat = compss_wait_on(adj_mat)
+    border_points = compss_wait_on(border_points)
     import copy
     tmp_mat = copy.deepcopy(adj_mat)
     for i in range(num_grid_rows):
@@ -257,10 +217,19 @@ def DBSCAN(epsilon, min_points):
                 neigh_squares.append(dataset[coord[0]][coord[1]])
             sync_clusters(dataset[i][j], adj_mat[i][j], epsilon, [i, j],
                           neigh_squares_loc,  *neigh_squares)
+
+    # Cluster list update
     adj_mat = compss_wait_on(adj_mat)
     links_list = unwrap_adj_mat(tmp_mat, adj_mat)
-#    links_list = update(links_list, adj_mat)
-    print links_list
+    for i in range(num_grid_rows):
+        for j in range(num_grid_cols):
+            neigh_sq_coord = neigh_squares_query([i, j], epsilon)
+            neigh_squares = []
+            for coord in neigh_sq_coord:
+                neigh_squares.append(dataset[coord[0]][coord[1]])
+            expand_cluster(dataset[i][j], epsilon, border_points[i][j],
+                           *neigh_squares)
+    print len(links_list)
     return links_list
 
 
