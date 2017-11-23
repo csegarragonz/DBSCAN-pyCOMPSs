762340480474951ac1729b5fb6747bb172224fe4
Carlos Segarra <carlossegarragonzalez@gmail.com>
2017-11-23 15:07:03 +0100

    Done with the first batch

--- DBSCAN_4_PyC.py
+++ DBSCAN_4_PyC.py
@@ -57,8 +57,8 @@ def neigh_squares_query(square, epsilon, dimensions):
     return tuple(neigh_squares)
 
 
-@task(returns=(Data, defaultdict, list, list))
-def partial_scan_merge(data, epsilon, min_points, adj_mat, *args):
+@task(returns=(Data, defaultdict, list, list, list))
+def partial_scan_merge(data, epsilon, min_points, *args):
     # Core point location in the data chunk
     data_copy = Data()
     data_copy.value = copy.deepcopy(data.value)
@@ -67,14 +67,16 @@ def partial_scan_merge(data, epsilon, min_points, adj_mat, *args):
     neigh_points = np.vstack(tmp_unwrap)
     neigh_points_clust = np.concatenate(tmp_unwrap_2)
     non_assigned = defaultdict(int)
-    for num, point in enumerate(data_copy.value[0]):
-        poss_neigh = np.linalg.norm(neigh_points - point, axis=1) - epsilon < 0
-        neigh_count = np.sum(poss_neigh)
+    dist_mat = np.linalg.norm(neigh_points -
+                              data_copy.value[0][:, np.newaxis, :],
+                              axis=2)-epsilon < 0
+    for num in range(dist_mat.shape[0]):
+        neigh_count = np.sum(dist_mat[num])
         if neigh_count > min_points:
             data_copy.value[1][num] = -1
         elif neigh_count > 0:
             tmp = []
-            for pos, proxim in enumerate(poss_neigh):
+            for pos, proxim in enumerate(dist_mat[num]):
                 if proxim:
                     # Adding three since later to detect core points we will
                     # require value > -1 and -0 is > 1 and not to confuse it
@@ -86,11 +88,31 @@ def partial_scan_merge(data, epsilon, min_points, adj_mat, *args):
                         tmp.append(pos)
             else:
                 non_assigned[num] = tmp
+#     for num, point in enumerate(data_copy.value[0]):
+#         poss_neigh = np.linalg.norm(neigh_points - point, axis=1) - epsilon < 0
+#         neigh_count = np.sum(poss_neigh)
+#         if neigh_count > min_points:
+#             data_copy.value[1][num] = -1
+#         elif neigh_count > 0:
+#             tmp = []
+#             for pos, proxim in enumerate(poss_neigh):
+#                 if proxim:
+#                     # Adding three since later to detect core points we will
+#                     # require value > -1 and -0 is > 1 and not to confuse it
+#                     # with a noise point
+#                     if neigh_points_clust[pos] == -1:
+#                         data_copy.value[1][num] = -(pos+3)
+#                         break
+#                     else:
+#                         tmp.append(pos)
+#             else:
+#                 non_assigned[num] = tmp
     # Cluster the core points found
     cluster_count = 0
     core_points = [[num, p] for num, p in enumerate(data_copy.value[0]) if
                    data_copy.value[1][num] == -1]
     for pos, point in core_points:
+        # TODO: make a one-liner out of this
         tmp_vec = (np.linalg.norm(data_copy.value[0]-point, axis=1)-epsilon) < 0
         for num, poss_neigh in enumerate(tmp_vec):
             if poss_neigh and data_copy.value[1][num] > -1:
@@ -99,7 +121,8 @@ def partial_scan_merge(data, epsilon, min_points, adj_mat, *args):
         else:
             data_copy.value[1][pos] = cluster_count
             cluster_count += 1
-    return data_copy, non_assigned, [cluster_count], [cluster_count]
+    return [data_copy, non_assigned, [cluster_count], [cluster_count],
+            dist_mat]
 #    return data, non_assigned
 
 
@@ -111,23 +134,27 @@ def dict_compss_wait_on(dicc, dimension_perms):
 
 @task(returns=list)
 # @task(adj_mat=INOUT)
-def sync_clusters(data, adj_mat, epsilon, coord, neigh_sq_loc, *args):
+def sync_clusters(data, adj_mat, epsilon, coord, neigh_sq_loc, dist_mat,
+                  *args):
+    # TODO: change *args
     adj_mat_copy = [[] for _ in range(max(adj_mat[0], 1))]
-    nice_args = [[args[i], neigh_sq_loc[i]] for i in range(len(neigh_sq_loc))]
-    for num1, point1 in enumerate(data.value[0]):
-        current_clust_id = int(data.value[1][num1])
+    tmp_unwrap_1 = [i.value[1] for i in args]
+    tmp_unwrap_1 = np.concatenate(tmp_unwrap_1)
+    tmp_unwrap_2 = [neigh_sq_loc[i] for i in range(len(neigh_sq_loc))
+                    for j in range(len(args[i].value[1]))]
+    nice_args = [tmp_unwrap_1, tmp_unwrap_2]
+    for num in range(dist_mat.shape[0]):
+        current_clust_id = int(data.value[1][num])
         if current_clust_id > -1:
-            for tmp2, loc2 in nice_args:
-                tmp_vec = (np.linalg.norm(tmp2.value[0]-point1, axis=1) -
-                           epsilon) < 0
-                for num2, poss_neigh in enumerate(tmp_vec):
-                    if poss_neigh:
-                        clust_ind = int(tmp2.value[1][num2])
-                        adj_mat_elem = [loc2, clust_ind]
-                        if ((clust_ind > -1) and
-                            (adj_mat_elem not in
-                             adj_mat_copy[current_clust_id])):
-                            adj_mat_copy[current_clust_id].append(adj_mat_elem)
+            # for tmp2, loc2 in nice_args:
+            for num2, poss_neigh in enumerate(dist_mat[num]):
+                if poss_neigh:
+                    clust_ind = int(nice_args[0][num2])
+                    adj_mat_elem = [nice_args[1][num2], clust_ind]
+                    if ((clust_ind > -1) and
+                        (adj_mat_elem not in
+                            adj_mat_copy[current_clust_id])):
+                        adj_mat_copy[current_clust_id].append(adj_mat_elem)
     return adj_mat_copy
 
 
@@ -140,8 +167,8 @@ def unwrap_adj_mat(tmp_mat, adj_mat, neigh_sq_coord, dimension_perms):
     for comb in itertools.product(*dimension_perms):
             # For an implementation issue, elements in adj_mat are
             # wrapped with an extra pair of [], hence the j[0]
-            for k in range(len(adj_mat[comb][0])-1):
-                mf_set.union(adj_mat[comb][0][k], adj_mat[comb][0][k+1])
+        for k in range(len(adj_mat[comb][0])-1):
+            mf_set.union(adj_mat[comb][0][k], adj_mat[comb][0][k+1])
     out = mf_set.get()
     return out
 
@@ -215,15 +242,18 @@ def DBSCAN(epsilon, min_points, file_id):
     dimension_perms = [range(i) for i in dimensions]
     dataset = defaultdict()
     dataset_tmp = defaultdict()
+    dist_mat = defaultdict()
     neigh_sq_coord = defaultdict()
     for comb in itertools.product(*dimension_perms):
-        dataset_tmp[comb] = Data()
         dataset[comb] = Data()
+        dataset_tmp[comb] = Data()
+        dist_mat[comb] = Data()
         init_data(dataset_tmp[comb], comb, file_id)
         neigh_sq_coord[comb] = neigh_squares_query(comb, epsilon,
                                                    dimensions)
 
     # Partial Scan And Initial Cluster merging
+    dataset_tmp = dict_compss_wait_on(dataset_tmp, dimension_perms)
     adj_mat = defaultdict()
     tmp_mat = defaultdict()
     border_points = defaultdict()
@@ -237,9 +267,10 @@ def DBSCAN(epsilon, min_points, file_id):
         [dataset[comb],
          border_points[comb],
          tmp_mat[comb],
-         adj_mat[comb]] = partial_scan_merge(dataset_tmp[comb],
-                                             epsilon, min_points,
-                                             *neigh_squares)
+         adj_mat[comb],
+         dist_mat[comb]] = partial_scan_merge(dataset_tmp[comb],
+                                              epsilon, min_points,
+                                              *neigh_squares)
 
     # Cluster Synchronisation
     for comb in itertools.product(*dimension_perms):
@@ -251,7 +282,7 @@ def DBSCAN(epsilon, min_points, file_id):
             neigh_squares.append(dataset[coord])
         # TODO: make as INOUT instead of OUT, currently not working
         adj_mat[comb] = sync_clusters(dataset[comb], adj_mat[comb], epsilon,
-                                      comb, neigh_squares_loc,
+                                      comb, neigh_squares_loc, dist_mat[comb],
                                       *neigh_squares)
 
     # Cluster list update
